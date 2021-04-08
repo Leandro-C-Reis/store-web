1adbb324767a82eed98a916ee7f268c2cce988dc
Leandro Christian dos Reis <leandroreis02@gmail.com>
2021-04-08 17:51:55 -0300

    build(header): create components properties

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import 'tailwindcss/tailwind.css';
+import Link from 'next/link';
 
 import CartIcon from '@/public/assets/cart';
 import PersonIcon from '@/public/assets/person';
@@ -6,33 +7,35 @@ import SearchIcon from '@/public/assets/search';
 
 const Header = () => {
     return (
-        <header 
-            className="w-full h-100px bg-white m-0 p-0 border-b border-gray-400 flex font-ubuntu">
+        <header
+            className="w-full h-100px bg-white m-0 p-0 border-b border-gray-400 flex font-ubuntu fixed">
             <div className="container bg-white mx-auto px-10 flex items-center justify-evenly">
-                <div className="bg-white w-300px h-60px border text-center">
-                    logo
-                </div>
+                <Link href="/">
+                    <a className="bg-white w-300px h-60px border text-center">
+                        logo
+                    </a>
+                </Link>
                 <div className="bg-white w-500px h-50px border rounded-2xl border-gray-300 flex justify-between items-center text-gray-500">
-                    <span className="ml-6 font-ubuntu">
-                        Encontre seus produtos...
-                    </span>
-                    <SearchIcon className="fill-current w-24px h-24px mr-6"/>
+                    <input className="ml-6 w-5/6 outline-none" placeholder="Encontre seus produtos..."/>
+                    <SearchIcon className="fill-current w-24px h-24px mr-6 cursor-pointer" />
                 </div>
                 <div className="bg-white h-auto w-auto flex align-center text-gray-500">
-                    <a className="flex align-center justify-center">
-
-                        <CartIcon className="fill-current mx-2 w-24px h-24px"/>
-                        <span>
-                            Meu Carrinho
-                        </span>
-                    </a>
-                    <a className="flex align-center justify-center ml-2">
-
-                        <PersonIcon className="fill-current mx-2 w-24px h-24px"/>    
-                        <span>
-                            Login | Cadastrar
-                        </span>
-                    </a>
+                    <Link href='/'>
+                        <a className="flex align-center justify-center">
+                            <CartIcon className="fill-current mx-2 w-24px h-24px" />
+                            <span>
+                                Meu Carrinho
+                            </span>
+                        </a>
+                    </Link>
+                    <Link href="/">
+                        <a className="flex align-center justify-center ml-2">
+                            <PersonIcon className="fill-current mx-2 w-24px h-24px" />
+                            <span>
+                                Login | Cadastrar
+                            </span>
+                        </a>
+                    </Link>
                 </div>
             </div>
         </header>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import 'tailwindcss/tailwind.css';
 import Header from '@/components/header';
 
 const Home = () => {
